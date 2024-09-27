e7dc00c73439ca189a14a05d7e717d1829312fc7
csae8092 <p.andorfer@gmail.com>
2024-09-27 17:13:26 +0200

    init commit

---
+++ webpage/metadata.py
@@ -0,0 +1,7 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'Bildzitate Datenbank',
+    'github': 'https://github.com/csae8092/bildzitate'
+}
